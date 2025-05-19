b773fb4af1fd438c3b94de918d1bd28fa938ca8d
Jose Moreno <jose.moreno39@uabc.edu.mx>
2025-05-19 15:45:19 -0700

    avance, funcional

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,8 +1,12 @@
 # app/routes/auth_routes.py
 from flask import Blueprint, render_template
 
-bp = Blueprint('auth', __name__)
+auth_routes = Blueprint('auth', __name__)
 
-@bp.route('/login')
+@auth_routes.route('/login')
 def login():
     return render_template('login.html')
+
+@auth_routes.route('/register')
+def register():
+    return render_template('register.html')
